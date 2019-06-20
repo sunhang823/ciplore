911dd565fb3edccd955017874b8a8c969e9497ff
Chris Sun <sunhang0823@gmail.com>
2019-06-20 16:41:56 -0400

    error message

--- ciploreBackEnd/src/main/java/com/cityexplore/ciplore/web/PropertyController.java
+++ ciploreBackEnd/src/main/java/com/cityexplore/ciplore/web/PropertyController.java
@@ -5,12 +5,16 @@ import com.cityexplore.ciplore.services.PropertyService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.validation.Valid;
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api/property")
@@ -20,8 +24,15 @@ public class PropertyController {
     PropertyService propertyService;
 
     @PostMapping("")
-    public ResponseEntity<Property> createNewProperty(@RequestBody Property property) {
+    public ResponseEntity<?> createNewProperty(@Valid @RequestBody Property property, BindingResult result) {
 
+        if(result.hasErrors()){
+            Map<String, String > errorMap = new HashMap<>();
+            for(FieldError error: result.getFieldErrors()) {
+                errorMap.put(error.getField(), error.getDefaultMessage());
+            }
+            return new ResponseEntity<Map<String, String>>(errorMap, HttpStatus.BAD_REQUEST);
+        }
         Property property1 = propertyService.saveOrUpdateProperty(property);
         return new ResponseEntity<Property>(property1, HttpStatus.CREATED);
     }
