bef879b206450a3c322eaaa54f510e88710d6287
Chris Sun <sunhang0823@gmail.com>
2019-06-20 15:56:16 -0400

    backend start

---
+++ ciploreBackEnd/src/main/java/com/cityexplore/ciplore/CiploreApplication.java
@@ -0,0 +1,13 @@
+package com.cityexplore.ciplore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CiploreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CiploreApplication.class, args);
+    }
+
+}
