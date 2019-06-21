fc5ec60327d5da9f81573618c90568b747858300
Chris Sun <sunhang0823@gmail.com>
2019-06-21 17:33:00 -0400

    nav bar

--- ciplore-frontend/src/components/Layout/Header.js
+++ ciplore-frontend/src/components/Layout/Header.js
@@ -1,11 +1,52 @@
 import React, { Component } from "react";
+import logo from "../../img/logo.png";
 
 class Header extends Component {
   render() {
     return (
-      <div>
-        <h1>NavBar</h1>
-      </div>
+      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
+        <div className="container">
+          <a class="navbar-brand mr-auto" href="#">
+            <img src={logo} alt="Logo" />
+          </a>
+          <a className="navbar-brand" href="Dashboard.html">
+            Ciplore
+          </a>
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#mobile-nav"
+          >
+            <span className="navbar-toggler-icon" />
+          </button>
+
+          <div className="collapse navbar-collapse" id="mobile-nav">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <a className="nav-link " href="register.html">
+                  Sign Up
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="login.html">
+                  Login
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="myAccount.html">
+                  My Account
+                </a>
+              </li>
+              <li className="nav-item">
+                <a className="nav-link" href="contact.html">
+                  Contact
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
     );
   }
 }
