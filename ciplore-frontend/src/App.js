7e5b55c2e7fb073a2665619c3bdf765e0a2954fc
Chris Sun <sunhang0823@gmail.com>
2019-06-21 17:43:02 -0400

    fix merge

--- ciplore-frontend/src/App.js
+++ ciplore-frontend/src/App.js
@@ -4,7 +4,11 @@ import PropertyDashboard from "./components/PropertyComponents/PropertyDashboard
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <PropertyDashboard />
+    </div>
+  );
 }
 
 export default App;
