5ef86f41747ba1bcc572f3016e5b8e3cd537bb61
Chris Sun <sunhang0823@gmail.com>
2019-06-21 15:50:22 -0400

    header

--- ciplore-frontend/src/components/PropertyComponents/PropertyDashboard.js
+++ ciplore-frontend/src/components/PropertyComponents/PropertyDashboard.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
 import PropertyItem from "./Property/PropertyItem";
+import Header from "../Layout/Header";
 
 class PropertyDashboard extends Component {
   render() {
     return (
       <div>
+        <Header />
         <h1>Welcome to Property Dashboard</h1>
         <PropertyItem />
       </div>
