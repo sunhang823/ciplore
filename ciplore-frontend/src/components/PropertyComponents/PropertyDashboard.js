06799472812e0d99c943b1a6a3d61f60327dc89f
Chris Sun <sunhang0823@gmail.com>
2019-06-21 13:30:39 -0400

    PropertyDashboard created

---
+++ ciplore-frontend/src/components/PropertyComponents/PropertyDashboard.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PropertyDashboard extends Component {
+  render() {
+    return <h1>Welcome to Property Dashboard</h1>;
+  }
+}
+
+export default PropertyDashboard;
