7e1ec3c948838c85bf12ced49adeddc2ba338516
Chris Sun <sunhang0823@gmail.com>
2019-06-21 15:48:17 -0400

    property item

---
+++ ciplore-frontend/src/components/PropertyComponents/Property/PropertyItem.js
@@ -0,0 +1,9 @@
+import React, { Component } from "react";
+
+class PropertyItem extends Component {
+  render() {
+    return <h1>Property Item</h1>;
+  }
+}
+
+export default PropertyItem;
--- ciplore-frontend/src/components/PropertyComponents/PropertyDashboard.js
+++ ciplore-frontend/src/components/PropertyComponents/PropertyDashboard.js
@@ -1,8 +1,14 @@
 import React, { Component } from "react";
+import PropertyItem from "./Property/PropertyItem";
 
 class PropertyDashboard extends Component {
   render() {
-    return <h1>Welcome to Property Dashboard</h1>;
+    return (
+      <div>
+        <h1>Welcome to Property Dashboard</h1>
+        <PropertyItem />
+      </div>
+    );
   }
 }
 
