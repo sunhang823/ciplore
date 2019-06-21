58fec2e90a22dd991348a413b66b0cd043c4b0fc
Chris Sun <sunhang0823@gmail.com>
2019-06-21 16:16:41 -0400

    bootstrap installed finally!!!!!

--- ciplore-frontend/src/App.js
+++ ciplore-frontend/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./App.css";
 import PropertyDashboard from "./components/PropertyComponents/PropertyDashboard";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
